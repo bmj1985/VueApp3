a1c661095dfabd2346fb58942b6316a10c656a74
Brandon Johnson <bmj1985@gmail.com>
2018-02-05 12:33:25 -0700

    copyed original repo files

---
+++ version-3/app/src/components/TheApplicaionForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id='TheApplicationForm'>
+    <form>
+      <label>Apply Here: </label>
+      <textarea id="application-text" rows="8" cols="100" v-model="applicationText"></textarea><br />
+      <input id="submit" type="submit" value="Submit" @click.prevent.reset="submitApplication" />
+    </form>
+    <p id="message">{{ message }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TheApplicationForm',
+  props: {
+    getText: {
+      type: Function,
+      required: true
+    }
+  },
+  data () {
+    return {
+      applicationText: '',
+      message: ''
+    }
+  },
+  watch: {
+    applicationText (newText) {
+      this.getText(newText)
+    }
+  },
+  methods: {
+    submitApplication(){
+      this.message = 'Your application was submitted!'
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
